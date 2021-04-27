0a09f0d8075b1235f3a5f5d6e6f13e908fda8721
s-dney <littlebirdsword@gmail.com>
2021-04-27 18:08:00 -0400

    create repository

---
+++ main.js
@@ -0,0 +1,202 @@
+
+function bardmenu(){
+document.querySelector(".bardoption").style.display="block"
+console.log("yes")
+}
+function bardmenu2(){
+    document.querySelector(".bardoption").style.display="none"
+}
+
+function bostonmenu(){
+document.querySelector(".bostonoption").style.display="block"
+console.log("yes")
+}
+function bostonmenu2(){
+    document.querySelector(".bostonoption").style.display="none"
+}
+
+function cccmenu(){
+document.querySelector(".cccoption").style.display="block"
+console.log("yes")
+}
+function cccmenu2(){
+    document.querySelector(".cccoption").style.display="none"
+}
+
+function hartfordmenu(){
+document.querySelector(".hartfordoption").style.display="block"
+console.log("yes")
+}
+function hartfordmenu2(){
+    document.querySelector(".hartfordoption").style.display="none"
+}
+
+function kentmenu(){
+document.querySelector(".kentoption").style.display="block"
+console.log("yes")
+}
+function kentmenu2(){
+    document.querySelector(".kentoption").style.display="none"
+}
+
+function mainemenu(){
+document.querySelector(".maineoption").style.display="block"
+console.log("yes")
+}
+function mainemenu2(){
+    document.querySelector(".maineoption").style.display="none"
+}
+
+function massartmenu(){
+document.querySelector(".massartoption").style.display="block"
+console.log("yes")
+}
+function massartmenu2(){
+    document.querySelector(".massartoption").style.display="none"
+}
+
+function nmsumenu(){
+document.querySelector(".nmsuoption").style.display="block"
+console.log("yes")
+}
+function nmsumenu2(){
+    document.querySelector(".nmsuoption").style.display="none"
+}
+
+function nyumenu(){
+document.querySelector(".nyuoption").style.display="block"
+console.log("yes")
+}
+function nyumenu2(){
+    document.querySelector(".nyuoption").style.display="none"
+}
+
+function osumenu(){
+document.querySelector(".osuoption").style.display="block"
+console.log("yes")
+}
+function osumenu2(){
+    document.querySelector(".osuoption").style.display="none"
+}
+
+function pcnwmenu(){
+document.querySelector(".pcnwoption").style.display="block"
+console.log("yes")
+}
+function pcnwmenu2(){
+    document.querySelector(".pcnwoption").style.display="none"
+}
+
+function risdmenu(){
+document.querySelector(".risdoption").style.display="block"
+console.log("yes")
+}
+function risdmenu2(){
+    document.querySelector(".risdoption").style.display="none"
+}
+
+function svamenu(){
+document.querySelector(".svaoption").style.display="block"
+console.log("yes")
+}
+function svamenu2(){
+    document.querySelector(".svaoption").style.display="none"
+}
+
+function syramenu(){
+document.querySelector(".syraoption").style.display="block"
+console.log("yes")
+}
+function syramenu2(){
+    document.querySelector(".syraoption").style.display="none"
+}
+
+function albanymenu(){
+document.querySelector(".albanyoption").style.display="block"
+console.log("yes")
+}
+function albanymenu2(){
+    document.querySelector(".albanyoption").style.display="none"
+}
+
+function unionmenu(){
+document.querySelector(".unionoption").style.display="block"
+console.log("yes")
+}
+function unionmenu2(){
+    document.querySelector(".unionoption").style.display="none"
+}
+
+function uconnmenu(){
+document.querySelector(".uconnoption").style.display="block"
+console.log("yes")
+}
+function uconnmenu2(){
+    document.querySelector(".uconnoption").style.display="none"
+}
+
+function umassdmenu(){
+document.querySelector(".umassdoption").style.display="block"
+console.log("yes")
+}
+function umassdmenu2(){
+    document.querySelector(".umassdoption").style.display="none"
+}
+
+function uscmenu(){
+document.querySelector(".uscoption").style.display="block"
+console.log("yes")
+}
+function uscmenu2(){
+    document.querySelector(".uscoption").style.display="none"
+}
+
+function utmenu(){
+document.querySelector(".utoption").style.display="block"
+console.log("yes")
+}
+function utmenu2(){
+    document.querySelector(".utoption").style.display="none"
+}
+
+function uartsmenu(){
+document.querySelector(".uartsoption").style.display="block"
+console.log("yes")
+}
+function uartsmenu2(){
+    document.querySelector(".uartsoption").style.display="none"
+}
+
+function yalemenu(){
+document.querySelector(".yaleoption").style.display="block"
+console.log("yes")
+}
+function yalemenu2(){
+    document.querySelector(".yaleoption").style.display="none"
+}
+
+$(document).on("scroll", function() {
+  var distanceScrolled =
+  $(document).scrollTop();
+  $("#letter1").css("top",distanceScrolled * 0.59);
+  $(document).scrollTop();
+  $("#letter2").css("top",distanceScrolled * 0.31);
+  $(document).scrollTop();
+  $("#letter3").css("top",distanceScrolled * 0.48);
+  $(document).scrollTop();
+  $("#letter4").css("top",distanceScrolled * 0.28);
+  $(document).scrollTop();
+  $("#letter5").css("top",distanceScrolled * 0.1);
+  $(document).scrollTop();
+  $("#letter6").css("top",distanceScrolled * 0.45);
+  $(document).scrollTop();
+  $("#letter8").css("top",distanceScrolled * 0.19);
+  $(document).scrollTop();
+  $("#letter9").css("top",distanceScrolled * 0.49);
+  $(document).scrollTop();
+  $("#letter10").css("top",distanceScrolled * 0.4);
+  $(document).scrollTop();
+  $("#letter11").css("top",distanceScrolled * 0.35);
+  $(document).scrollTop();
+  $("#letter12").css("top",distanceScrolled * 0.52);
+})
